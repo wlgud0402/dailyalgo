545c7061fb2411d8b0431d2aa1f16b2efcb26e03
wlgud0402 <wlgudrlgus@naver.com>
2020-05-03 23:50:17 +0900

    2020-05-03

---
+++ daily_3_.py
@@ -0,0 +1,20 @@
+def isOdd_Even(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+# def double_list(list_x, list_y):
+#     answer =[]
+    # union = [x+y for x,y in zip(list_x, list_y)]
+#     return answer.append(union)
+
+# list_x=[[1,2],[2,3]]
+# list_y=[[3,4],[5,6]]
+# # # [[1,2],[2,3]]	[[3,4],[5,6]]	[[4,6],[7,9]]
+# # print(double_list(list_x, list_y))
+# def a(list_x, list_y):
+#     union = [x+y for x,y in zip(list_x, list_y)]
+#     return union
+# print(a(list_x, list_y))
+
