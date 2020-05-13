fe1f1c76cda1659c2e9b28ac1621e3fc9734b9bd
wlgud0402 <wlgudrlgus@naver.com>
2020-05-14 00:14:18 +0900

    2020년 05월 14일 목 오전 12:14:18

--- 체육복도난.py
+++ 체육복도난.py
@@ -65,14 +65,14 @@
 # def burrow(n, lost, reserve):
 #     in
 
-b1 = set([1,2,3,4,5])
-b2 = set([3,5])
+# b1 = set([1,2,3,4,5])
+# b2 = set([3,5])
 
-b3= b1.intersection(b2)
+# b3= b1.intersection(b2)
 
-b4 = b1.difference(b3)
+# b4 = b1.difference(b3)
 
-print(b4)
+# print(b4)
 
 def solution(n, lost, reserve):
     lost = set(lost)
@@ -80,8 +80,53 @@ def solution(n, lost, reserve):
     common = lost.intersection(reserve)
 
     lost = list(lost.difference(common))
+    find = set()
     reserve = list(reserve.difference(common))
 
-    
+    for lost_stu in lost:
+        front_stu = lost_stu - 1
+        back_stu = lost_stu + 1
+        if front_stu in reserve:
+            # lost.remove(lost_stu)
+            find.add(lost_stu)
+            reserve.remove(front_stu)                
+            continue
+        if back_stu in reserve:
+            # lost.remove(lost_stu)
+            find.add(lost_stu)
+            reserve.remove(back_stu)
+            continue
+        
 
-    
+    return n - len(lost) + len(find)
+
+def solution2(n, lost, reserve):
+    lost = set(lost)
+    reserve = set(reserve)
+    find = set()
+
+    for lost_stu in lost:
+        front_stu = lost_stu - 1
+        back_stu = lost_stu + 1
+        if lost_stu in reserve:
+            find.add(lost_stu)
+            reserve.remove(lost_stu)                
+            continue
+        if front_stu in reserve:
+            # lost.remove(lost_stu)
+            find.add(lost_stu)
+            reserve.remove(front_stu)                
+            continue
+        if back_stu in reserve:
+            # lost.remove(lost_stu)
+            find.add(lost_stu)
+            reserve.remove(back_stu)
+            continue
+        
+
+    return n - len(lost) + len(find)
+
+print(solution2(5, [2, 4], [1, 3, 5])) #5
+print(solution2(5, [2, 4], [3])) #4
+print(solution2(3, [3], [1])) #2
+print(solution2(5, [2,3], [2,3]))
