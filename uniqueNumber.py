66229a901cbea9aa099c1b68fc8ebdac68c05b7b
wlgud0402 <wlgudrlgus@naver.com>
2020-05-28 23:16:14 +0900

    2020년 05월 28일 목 오후 11:16:14

--- uniqueNumber.py
+++ uniqueNumber.py
@@ -2,7 +2,7 @@ class Solution:
     def uniqueOccurrences(self, arr):
         count = {}
         for num in arr:
-            try: 
+            try:    #count딕트에 num을 키로 가지고 있는곳에 1을 추가해주는 것을 시도 BUT 없다면 이제 한개추가되는것 => 오류 => except문 실행
                 count[num] += 1
             except:
                 count[num] = 1
