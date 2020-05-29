fbdd434f58aff44539fdb7e67f33743cf3a65d95
wlgud0402 <wlgudrlgus@naver.com>
2020-05-29 17:34:38 +0900

    2020년 05월 29일 금 오후  5:34:38

---
+++ BuildAnArray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def buildArray(self, target, n):
+        answer = []
+        for i in range(len(target)):
+            try:
+                if target[i] == i+1
+        
+
+solution=Solution()
+
+print(solution.buildArray([1,2,3],3))
+#출력 : ["Push","Push","Push"]
