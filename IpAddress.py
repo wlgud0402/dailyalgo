553b89314c72ccac93c22e7a63cc733662be20fe
wlgud0402 <wlgudrlgus@naver.com>
2020-06-04 14:07:28 +0900

    2020년 06월  4일 목 오후  2:07:28

--- IpAddress.py
+++ IpAddress.py
@@ -2,12 +2,20 @@ class Solution:
     def defangIPaddr(self, address):
         answer = ""
         for s in address:
+            if s == ".":
+                answer += "[.]"
+            else:
+                answer += s
+            """
             if s != ".":
                 answer += s
             else:
                 answer += "[.]"
+            """
         return answer
 
 solution=Solution()
 print(solution.defangIPaddr("1.1.1.1"))
         
+
+# print("1.1.1.1".replace(".", "[.]"))
