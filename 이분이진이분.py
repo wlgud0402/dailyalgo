a9440de888f09c286cf4fd3d277c038f41eb7111
wlgud0402 <wlgudrlgus@naver.com>
2020-06-14 23:44:24 +0900

    2020년 06월 14일 일 오후 11:44:24

--- 이분이진이분.py
+++ 이분이진이분.py
@@ -1,2 +1,7 @@
-def solution():
-    return "hi"
+def divide_search():
+    for i in range(2,10):
+        for j in range(1,10):
+            print("{} * {} = {} ".format(i,j,i*j))
+
+
+divide_search()
