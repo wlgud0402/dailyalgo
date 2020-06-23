3348e2e2af0224e9747d8f3cd2bfcfe040dce145
wlgud0402 <wlgudrlgus@naver.com>
2020-06-23 15:44:09 +0900

    2020년 06월 23일 화 오후  3:44:09

---
+++ 다트게임.py
@@ -0,0 +1,15 @@
+
+def solution(dartResult):
+    answer = []
+    for i in range(len(dartResult)):
+        
+        
+
+    return answer
+
+    
+
+print(solution("1S2D*3T"))
+
+#최종 값=37	
+# 11 * 2 + 22 * 2 + 33
