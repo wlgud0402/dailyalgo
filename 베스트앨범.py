d75c8bdc80b38babfabc20b92248312114350e63
wlgud0402 <wlgudrlgus@naver.com>
2020-07-10 00:03:00 +0900

    2020년 07월 10일 금 오전 12:02:59

---
+++ 베스트앨범.py
@@ -0,0 +1,21 @@
+def solution(genres, plays):
+    plays_count = {}
+    answer = []
+    genres_plays_sets = list(zip(genres, plays))
+
+    for genres_plays_set in genres_plays_sets:
+        key = genres_plays_set[0]
+        try:
+            if plays_count[key]:
+                plays_count[key] += genres_plays_set[1]
+        except:
+            plays_count[key] = genres_plays_set[1]
+
+    
+
+    return plays_count
+
+
+
+
+print(solution(["classic", "pop", "classic", "classic", "pop"],[500, 600, 150, 800, 2500]))
