f56a974da4dac25e831217d130f86316a7e429af
wlgud0402 <wlgudrlgus@naver.com>
2020-07-11 00:06:00 +0900

    2020년 07월 11일 토 오전 12:05:59

--- 영어끝말잇기.py
+++ 영어끝말잇기.py
@@ -1,15 +1,23 @@
 def solution(n, words):
-    said_word = words[0]
-
-    said_words = ""
+    said_words = words[0]
 
     for i in range(1, len(words)):
-        if said_word[-1] != words[i][0] or words[i] in said_words:
-            return "여기서 몇번째의 사람이 몇번차때 실수"
+        if said_words[-1] != words[i][0] or words[i] in said_words:
+            return [(i%n)+1, (i//n)+1]
         else:
             said_words += ","
             said_words += words[i]
-    return [0,0]
+    return [0,0]  
 
 
 print(solution(3,["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]))
+
+print()
+print()
+
+print(solution(5,["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"]))
+
+print()
+print()
+
+print(solution(2,["hello", "one", "even", "never", "now", "world", "draw"]))
