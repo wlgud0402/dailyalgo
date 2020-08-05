73559f54891df0a58d77b3e5cc7d4806e0b0bfc1
wlgud0402 <wlgudrlgus@naver.com>
2020-08-05 23:42:26 +0900

    Wed, Aug  5, 2020 11:42:26 PM

--- 짝지어제거하기.py
+++ 짝지어제거하기.py
@@ -1,7 +1,24 @@
-def solution(s):
-    answer = 0
+import copy
 
-    return answer
+def solution(strings):
+    answers = [strings[0]]
+    for i in range(1, len(strings)):
+        answers.append(strings[i])
+        new_answer = copy.deepcopy(answers)
+        for j in range(len(answers)):
+            try:
+                if new_answer[j] == new_answer[j+1]:
+                    new_answer.pop(j+1)
+                    new_answer.pop(j)
+
+                    answers = new_answer
+            except:
+                "에러요"
+
+    if len(answers) == 0:
+        return 1
+    else:
+        return 0
 
 
 
