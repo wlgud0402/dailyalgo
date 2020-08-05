ba4376abc949164562526c036240ea9a0c60c480
wlgud0402 <wlgudrlgus@naver.com>
2020-08-05 23:22:49 +0900

    Wed, Aug  5, 2020 11:22:49 PM

---
+++ _h_CreditCardNumber.py
@@ -0,0 +1,9 @@
+def credit_number(numbers):
+    same = {}
+    if len(str(numbers)) == 16 or len(str(numbers)) == 19:
+        for num in str(numbers):
+            
+
+
+
+print(credit_number(4253625879615782))
---
+++ 짝지어제거하기.py
@@ -0,0 +1,9 @@
+def solution(s):
+    answer = 0
+
+    return answer
+
+
+
+print(solution("baabaa")) #1
+print(solution("cdcd")) #0
