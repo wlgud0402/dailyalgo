ffe2413ab53e423276a33fd47d51bcf1e897908b
wlgud0402 <wlgudrlgus@naver.com>
2020-08-20 02:08:14 +0900

    Thu, Aug 20, 2020  2:08:14 AM

--- ThreeSum.py
+++ ThreeSum.py
@@ -13,8 +13,28 @@ class Solution:
 
 
 
+    def threeSum2(self, nums):
+        nums.sort()
+        answer = []
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if -(nums[i] + nums[j]) in nums[j+1:] and [nums[i], nums[j], -(nums[i] + nums[j])] not in answer:
+                    answer.append([nums[i], nums[j], -(nums[i] + nums[j])])
 
+        return answer
+
+    def threeSum3(self, nums):
+        nums.sort()
+        answer = []
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if -(nums[i] + nums[j]) in nums[j+1:]:
+                    possible = [nums[i], nums[j], -(nums[i] + nums[j])]
+                    answer.append(possible)
+
+        tuple_answer = set(map(tuple, answer))
+        return [list(item) for item in tuple_answer]
 
 solution = Solution()
-print(solution.threeSum([-1, 0, 1, 2, -1, -4]))
+print(solution.threeSum3([-1, 0, 1, 2, -1, -4]))
         
