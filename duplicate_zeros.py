6d9c8b7abc70d3ff23927b80bbf1e69fe5ea1d4a
wlgud0402 <wlgudrlgus@naver.com>
2020-08-23 00:16:48 +0900

    Sun, Aug 23, 2020 12:16:48 AM

--- duplicate_zeros.py
+++ duplicate_zeros.py
@@ -1,27 +1,40 @@
+#리턴없이 원래 arr를 바꾸기
+#원래 길이 초과할수 x
 import copy
-
 class Solution:
-    def duplicateZeros(self, arr):
-        count = len(arr)
-        new_arr = copy.deepcopy(arr)
-        for i in range(count):
-            if arr[i] == 0:
-                new_arr.insert(i+1, 0)
+    # def duplicateZeros(self, numbers):
+    #     length = len(numbers)
+    #     zero_idx = []
+    #     for i, num in enumerate(numbers):
+    #         if num == 0:
+    #             zero_idx.append(i)
         
-        arr = new_arr[:count]
-        print(arr)
+    #     for zero in zero_idx:
+    #         numbers.insert(zero+1, 0)
+
+    #     numbers = numbers[:length]
+    #     print(numbers)
+
+    def duplicateZeros2(self, numbers):
+        length = len(numbers)
+        answer = []
+        for i in range(len(numbers)):
+            if numbers[i] == 0:
+                  answer.append(0)
+                  answer.append(0)
+            else :
+                answer.append(numbers[i])
+        
+        numbers = answer[:length]
+        print(numbers)
+        
+
+#[1,0,0,2,3,0,0,4]
+
+
+
+
 
 
 solution = Solution()
-print(solution.duplicateZeros([1,0,2,3,0,4,5,0]))
-print(solution.duplicateZeros([1,2,3]))
-print(solution.duplicateZeros([1,0,3,0,2,2,0]))
-
-# => [1,0,0,2,3,0,0,4]
-
-
-
-
-
-#리턴없이 원래 arr를 바꾸기
-#원래 길이 초과할수 x
+print(solution.duplicateZeros2([1,0,2,3,0,4,5,0]))
