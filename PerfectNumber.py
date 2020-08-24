67f8dc723434612b195c140887dc9ee9a2db8d43
wlgud0402 <wlgudrlgus@naver.com>
2020-08-25 00:30:33 +0900

    Tue, Aug 25, 2020 12:30:33 AM

--- PerfectNumber.py
+++ PerfectNumber.py
@@ -1,8 +1,8 @@
 class Solution:
     def checkPerfectNumber(self, num):
         divide_nums = []
-        for i in range(1 , (num + 1)):
-            if num % i == 0 and num != i:
+        for i in range(1 , (num)):
+            if num % i == 0:
                 divide_nums.append(i)
 
         if sum(divide_nums) == num:
@@ -10,21 +10,16 @@ class Solution:
         else:
             return False
 
+    def checkPerfectNumber2(self, num):
+        sum = 0
+        for i in range(1 , num):
+            if num % i == 0:
+                sum += i
+
+        return sum == num
+
+
 solution = Solution()
 print(solution.checkPerfectNumber(28))
+print(solution.checkPerfectNumber2(28))
 
-# class Solution:
-#     def checkPerfectNumber(self, num):
-#         divide_num = 0
-#         for i in range(1 , (num + 1)):
-#             if num % i == 0 and num != i:
-#                 divide_num += i
-
-#         if divide_num == num:
-#             return True
-#         else:
-#             return False
-
-# solution = Solution()
-# print(solution.checkPerfectNumber(28))
-        
