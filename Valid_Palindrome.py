0849f280c8bb869554fbf4436dd55c5728d70e68
wlgud0402 <wlgudrlgus@naver.com>
2020-08-30 22:39:24 +0900

    Sun, Aug 30, 2020 10:39:24 PM

--- Valid_Palindrome.py
+++ Valid_Palindrome.py
@@ -1,12 +1,21 @@
 class Solution:
     def isPalindrome(self, string):
         string_alnum = "".join([s.lower() for s in string if s.isalnum()])
-        
+        return string_alnum == string_alnum[::-1]
         # if string_alnum == string_alnum[::-1]:
         #     return True
         # else:
         #     return False
-        return string_alnum == string_alnum[::-1]
+        
+
+        # mid = len(string_alnum) / 2
+        # left = 0
+        # while left < mid:
+        #     right = len(string_alnum) - 1 - left
+        #     if string_alnum[left] != string_alnum[right]:
+        #         return False
+        #     left += 1
+        # return True
 
 
 solution = Solution()
