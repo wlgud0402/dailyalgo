7d861e9443080544c219d52dea68b7bdfd129910
wlgud0402 <wlgudrlgus@naver.com>
2020-09-29 19:40:46 +0900

    failed

---
+++ programmers/H-index.py
@@ -0,0 +1,14 @@
+import copy
+
+
+def solution(citations):
+    citations.sort()
+    # for i in range(len(citations)):
+    #     posible_index = copy.deepcopy(citations)
+    #     for j in range(len(citations)):
+    #         if
+    print(citations)
+
+
+print(solution([3, 0, 6, 1, 5]))
+# return 3
