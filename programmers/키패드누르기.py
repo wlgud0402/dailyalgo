e3f437a35385a748967fa082279bbd88f6255b8b
wlgud0402 <wlgudrlgus@naver.com>
2020-10-01 21:50:42 +0900

    failed

---
+++ programmers/키패드누르기.py
@@ -0,0 +1,21 @@
+# 1, 4, 7, * left hand
+
+# 3, 6, 9, # right hand
+
+# 2, 5 ,8 ,0 거리에따라
+
+# 키패드 => [[1,2,3],
+#          [4,5,6],
+#          [7,8,9],
+#          ["*",0,"#"]]
+def solution(numbers, hand):
+    close_len = 0
+    position = {
+        "left": "*",
+        "right": "#"
+    }
+    answer = ''
+    return answer
+
+
+print(solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], "right"))
