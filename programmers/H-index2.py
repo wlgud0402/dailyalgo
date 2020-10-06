a1c39f50f447acb9dbb2c3f56c9e0c6844eaea1c
wlgud0402 <wlgudrlgus@naver.com>
2020-10-06 18:28:07 +0900

    failed

---
+++ programmers/H-index2.py
@@ -0,0 +1,29 @@
+# def solution(citations):
+#     citations.sort()
+
+#     H = citations[len(citations)//2]
+#     for i in range(len(citations)):
+#         if len(citations[:H]) >= H:
+#             return H
+#         else:
+#             H += 1
+#     return H
+
+def solution(citations):
+    citations.sort()
+    H = citations[len(citations)//2]
+    # large = 0
+    # small = 0
+    for i in range(len(citations)):
+        large = [i for i in citations if i >= H]
+        small = [i for i in citations if i < H]
+
+        if len(large) >= H and len(small) < H:
+            return H
+        else:
+            H += 1
+    return H
+
+
+print(solution([3, 0, 6, 1, 5]))
+# 3
