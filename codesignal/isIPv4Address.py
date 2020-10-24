7168b0e217403af77a3abc174ca3d19386058ff7
wlgud0402 <wlgudrlgus@naver.com>
2020-10-24 17:00:35 +0900

    use isdigit

--- codesignal/isIPv4Address.py
+++ codesignal/isIPv4Address.py
@@ -1,31 +1,16 @@
-# def isIPv4Address(inputString):
-#     inputString = inputString.split(".")
-#     print(inputString)
-#     address = [i for i in inputString if i != '']
-#     print(address)
-#     only_num = [int(i) for i in address if 0 < int(i) < 255]
-
-#     if len(only_num) == 4:
-#         return True
-#     else:
-#         return False
-
 def isIPv4Address(inputString):
     inputString = inputString.split(".")
-    address = [i for i in inputString]
     answer = []
-    for add in address:
-        try:
-            if int(add).isdigit() and 0 < int(i) < 255:
-                answer.qppene(int(dd))
-        except:
+    for address in inputString:
+        if address.isdigit() and len(address) == len(str(int(address))) and 0 <= int(address) <= 255:
+            answer.append(address)
+        else:
             return False
 
-    print(answer)
-    if len(answer) == 0:
+    if len(answer) == 4:
         return True
     else:
         return False
 
 
-print(isIPv4Address("172.16.254.1"))
+print(isIPv4Address("01.254.255.0"))
