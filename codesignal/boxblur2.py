e45bbbc061c61a7553f82cb456fda7cacf992c6a
wlgud0402 <wlgudrlgus@naver.com>
2020-11-01 19:16:40 +0900

    solved

--- codesignal/boxblur2.py
+++ codesignal/boxblur2.py
@@ -1,21 +1,26 @@
 # -*- coding: utf-8 -*-
 def boxBlur(image):
-    box_size = len(image[0])-2
+    if len(image[0]) > len(image):
+        box_size = len(image[0])-2
+    else:
+        box_size = len(image)-2
     new_box = []
     for i in range(1, box_size+1):
         one_box = []
         for j in range(1, box_size+1):
-            print(i, j)
-    #         try:
-    #             new_point = int(sum(image[i-1][j-1:j+2]
-    #                                 + image[i][j-1:j+2]
-    #                                 + image[i+1][j-1:j+2])/9)
-    #             one_box.append(new_point)
-    #         except:
-    #             break
-    #     if one_box != []:
-    #         new_box.append(one_box)
-    # return new_box
+            try:
+                new_point = int((image[i-1][j-1] + image[i-1][j] + image[i-1][j + 1]
+                                 + image[i][j-1]+image[i][j]+image[i][j+1]
+                                 + image[i+1][j-1]+image[i+1][j]+image[i+1][j+1])/9)
+
+                one_box.append(new_point)
+            except:
+                break
+
+        if one_box != []:
+            new_box.append(one_box)
+
+    return new_box
 
 
 # print(boxBlur([[7, 4, 0, 1],
@@ -25,10 +30,9 @@ def boxBlur(image):
 
 # [[5, 4],
 # [4, 4]]
-
-# print(boxBlur([[36, 0, 18, 9],
-#                [27, 54, 9, 0],
-#                [81, 63, 72, 45]]))
+print(boxBlur([[36, 0, 18, 9],
+               [27, 54, 9, 0],
+               [81, 63, 72, 45]]))
 
 # [[40,30]]
 
