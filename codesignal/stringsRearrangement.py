b14a85ff019b126c72525623e93aa5b3d439ec19
wlgud0402 <wlgudrlgus@naver.com>
2020-11-08 13:12:58 +0900

    again failed

--- codesignal/stringsRearrangement.py
+++ codesignal/stringsRearrangement.py
@@ -3,7 +3,6 @@ import itertools
 
 def stringsRearrangement(inputArray):
     nPr = list(itertools.permutations(inputArray, len(inputArray)))
-    print(nPr)
 
     for array in nPr:
         flag = True
@@ -15,9 +14,7 @@ def stringsRearrangement(inputArray):
                         flag = False
                         break
 
-                    if array[i][j] == array[i+1]:
-                        continue
-                    else:
+                    if array[i][j] != array[i+1][j]:
                         diff_count += 1
             else:
                 break
@@ -26,6 +23,28 @@ def stringsRearrangement(inputArray):
             return True
         else:
             return False
+
+    # for array in nPr:
+    #     flag = True
+    #     for i in range(len(array)-1):
+    #         diff_count = 0
+    #         if flag:
+    #             for j in range(len(array[0][0])):
+    #                 if diff_count >= 2:
+    #                     flag = False
+    #                     break
+
+    #                 if array[i][j] == array[i+1]:
+    #                     continue
+    #                 else:
+    #                     diff_count += 1
+    #         else:
+    #             break
+
+    #     if flag:
+    #         return True
+    #     else:
+    #         return False
 
 
 print(stringsRearrangement(["ab", "bb", "aa"]))
