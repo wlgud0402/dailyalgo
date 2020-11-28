5eee590664caaf3df2d688197afa2f1390b06b31
wlgud0402 <wlgudrlgus@naver.com>
2020-11-28 15:21:05 +0900

    what is problem

--- codesignal/longestWord2.py
+++ codesignal/longestWord2.py
@@ -1,14 +1,29 @@
 def longestWord(text):
-    text = text.split()
-    text = [list(i) for i in text]
-    for i in range(len(text)):
-        for s in text[i]:
-            if s.isalpha():
+    string_list = ""
+    for string in text:
+        if ("a" <= string <= "z") or ("A" <= string <= "Z"):
+            string_list += string
+        else:
+            if string_list[-1] == " ":
                 continue
             else:
-                text[i].remove(s)
+                string_list += " "
+    split_string_list = string_list.split()
 
-    print(text)
+    max_len = len(split_string_list[0])
+    max_str = split_string_list[0]
+
+    for string in split_string_list:
+        if len(string) > max_len:
+            max_str = string
+            max_len = len(string)
+    return max_str
 
 
-print(longestWord("Ready, steady, go!"))
+print(longestWord("You are the best!!!!!!!!!!![[!]] CodeFighter ever!"))
+
+# string = "D"
+# if string > "F":
+#     print("big")
+# else:
+#     print("small")
