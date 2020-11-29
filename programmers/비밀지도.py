c282badd4ba93340652929eb0348fb8adde49499
wlgud0402 <wlgudrlgus@naver.com>
2020-11-29 18:11:45 +0900

    use rjust

--- programmers/비밀지도.py
+++ programmers/비밀지도.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+#rjust , zfill
+# rjust는 원하는 만큼의 값을 채워줌 <<앞에>> rjust(width, [fillchar])
+# ljust는 <<뒤에>>
+# center는 값을 가운데두고 <<원하는 문자열을 가운데에>>
+# zfill width만큼의 0 을 앞에 붙여줌 zfill(width)
+
 def solution(n, arr1, arr2):
     bin_arr1 = []
     bin_arr2 = []
@@ -35,6 +42,21 @@ def solution(n, arr1, arr2):
     for i in range(len(new_map)):
         new_map[i] = ''.join(new_map[i])
     return new_map
+
+# use rjust
+
+
+def solution2(n, arr1, arr2):
+    answer = []
+    for i, j in zip(arr1, arr2):
+        a12 = str(bin(i | j)[2:])
+        # 2진수화 했을때 두개를 비교하고 하나라도 1일경우 1로 나오게된다
+        # ex ) bin(20) => 10100 , bin(1) => 00001 bin(20|1) => 11111
+        a12 = a12.rjust(n, '0')
+        a12 = a12.replace('1', '#')
+        a12 = a12.replace('0', ' ')
+        answer.append(a12)
+    return answer
 
 
 # # ["#####","# # #", "### #", "# ##", "#####"]
