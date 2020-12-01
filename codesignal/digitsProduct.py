ba086f8a913a53b08e3a31d228d7b12cc05f8ed4
wlgud0402 <wlgudrlgus@naver.com>
2020-12-01 18:34:04 +0900

    doing solving

--- codesignal/digitsProduct.py
+++ codesignal/digitsProduct.py
@@ -1,5 +1,10 @@
 def digitsProduct(product):
-    pass
+    div = []
+    for i in range(1, product+1):
+        if product % i == 0:
+            div.append(i)
+
+    return div
 
 
 print(digitsProduct(12))  # 26
