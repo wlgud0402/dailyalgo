09795357172c5a9cafd95521b52d4c9e4d86a84b
wlgud0402 <wlgudrlgus@naver.com>
2020-12-04 15:51:48 +0900

    failed

--- codesignal/digitsProduct.py
+++ codesignal/digitsProduct.py
@@ -1,32 +1,33 @@
-# import itertools
-
-
 # def digitsProduct(product):
-#     div = []
-#     for i in range(2, product):
-#         while True:
+#     divisors = []
+#     div = 2
 
-#         if product % i == 0:
-#             div.append(i)
+#     while product != 1:
+#         if product % div == 0:
+#             print(div)
+#             product = product/div
+#             divisors.append(div)
+#         else:
+#             div += 1
 
-#     # bundle = list(itertools.permutations(div, len(div)))
-#     # print(bundle)
+#     return divisors
 
 
 # print(digitsProduct(12))  # 26
+
 def digitsProduct(product):
-    divisors = []
-    div = 2
+    for i in range(1, 10000):
+        number = str(i)
+        total = 1
 
-    while product != 1:
-        if product % div == 0:
-            print(div)
-            product = product/div
-            divisors.append(div)
-        else:
-            div += 1
+        for char in number:
+            total *= int(char)
 
-    return divisors
+        if total == product:
+            return i
+
+    # if not found return -1
+    return -1
 
 
-print(digitsProduct(12))  # 26
+print(digitsProduct(12))
