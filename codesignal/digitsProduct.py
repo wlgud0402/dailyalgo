ba958bf1d75abeeeffc147a6db092cadc83e63b8
wlgud0402 <wlgudrlgus@naver.com>
2020-12-04 15:32:39 +0900

    failed

--- codesignal/digitsProduct.py
+++ codesignal/digitsProduct.py
@@ -1,16 +1,32 @@
-import itertools
+# import itertools
 
 
+# def digitsProduct(product):
+#     div = []
+#     for i in range(2, product):
+#         while True:
+
+#         if product % i == 0:
+#             div.append(i)
+
+#     # bundle = list(itertools.permutations(div, len(div)))
+#     # print(bundle)
+
+
+# print(digitsProduct(12))  # 26
 def digitsProduct(product):
-    div = []
-    for i in range(2, product):
-        while True:
+    divisors = []
+    div = 2
 
-        if product % i == 0:
-            div.append(i)
+    while product != 1:
+        if product % div == 0:
+            print(div)
+            product = product/div
+            divisors.append(div)
+        else:
+            div += 1
 
-    # bundle = list(itertools.permutations(div, len(div)))
-    # print(bundle)
+    return divisors
 
 
 print(digitsProduct(12))  # 26
