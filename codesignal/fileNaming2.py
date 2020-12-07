70065e19e92b9cb9a82321789dd0dc77e3d581f8
wlgud0402 <wlgudrlgus@naver.com>
2020-12-07 16:45:04 +0900

    success

--- codesignal/fileNaming2.py
+++ codesignal/fileNaming2.py
@@ -1,38 +1,32 @@
+# def fileNaming(names):
+#     answer = []
+#     count = {}
+#     for name in names:
+#         if name in count:
+#             count[name] += 1
+#             new = name+"("+str(count[name])+")"
+#             if new in count:
+#                 while new in count:
+#                     count[name] += 1
+#                     new = name+"("+str(count[name])+")"
+#                 answer.append(new)
+#                 count[new] = 0
+#                 continue
+#             answer.append(new)
+#             count[new] = 0
+#         else:
+#             answer.append(name)
+#             count[name] = 0
+#     return answer
+
 def fileNaming(names):
-    answer = []
-    count = {}
-    for name in names:
-        if name in count:
-            count[name] += 1
-            new = name+"("+str(count[name])+")"
-            if new in count:
-                while new in count:
-                    count[name] += 1
-                    new = name+"("+str(count[name])+")"
-                answer.append(new)
-                count[new] = 0
-                continue
-            answer.append(new)
-            count[new] = 0
-        else:
-            answer.append(name)
-            count[name] = 0
-    return answer
-
-    # answer = []
-    # count = {}
-    # for name in names:
-    #     if name in count:
-
-    #         count[name] += 1
-    #         new = name+"("+str(count[name])+")"
-    #         answer.append(new)
-    #         count[new] = 0
-    #     else:
-    #         answer.append(name)
-    #         count[name] = 0
-    # print(count)
-    # return answer
+    for i in range(len(names)):
+        if names[i] in names[:i]:
+            j = 1
+            while names[i]+"("+str(j)+")" in names[:i]:
+                j += 1
+            names[i] += "("+str(j)+")"
+    return names
 
 
 print(fileNaming(["doc", "doc", "image", "doc(1)", "doc"]))
