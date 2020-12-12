ed28f9dad706095a6fa50a199e6898906e4fa496
wlgud0402 <wlgudrlgus@naver.com>
2020-12-12 17:47:41 +0900

    failed

---
+++ programmers/행렬의 곱셈.py
@@ -0,0 +1,9 @@
+def solution(arr1, arr2):
+    new = [[-1] * len(arr2[0]) for i in range(len(arr1))]
+    # answer = [[]]
+    # return answer
+    print(new)
+
+
+print(solution([[1, 4], [3, 2], [4, 1]]	, [[3, 3], [3, 3]]))
+#[[15, 15], [15, 15], [15, 15]]
