1c21372e8e0c61d825169489f190374ee64f3580
wlgud0402 <wlgudrlgus@naver.com>
2020-12-12 16:33:56 +0900

    failed

--- codesignal/practice/firstDuplicate.py
+++ codesignal/practice/firstDuplicate.py
@@ -1,10 +1,26 @@
+import collections
+# 3이상인 경우도 생각해야한다.
+
+
 def firstDuplicate(array):
-    duplicate = -1
-    for i in range(len(array)-1, 0, -1):
-        if array[i] in array[:i]:
-            duplicate = array[i]
+    if len(array) == len(set(array)):
+        return -1
+    count = collections.Counter(array)
+    idx = 0
+    number = array[0]
 
-    return duplicate
+    for i in range(1, len(array)):
+        if array[i] == number:
+            return array[i]
 
+        if count[array[i]] >= 2:
+            number = array[i]
 
+    # if len(array) == len(set(array)):
+    #     return False
+    # duplicate = -1
+    # for i in range(len(array)-1, 0, -1):
+    #     if array[i] in array[:i]:
+    #         duplicate = array[i]
+    # return duplicate
 print(firstDuplicate([2, 1, 3, 5, 3, 2]))  # 3
