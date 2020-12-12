49ee2fb985d24ddfaa67c9bb4586d92631ac6500
wlgud0402 <wlgudrlgus@naver.com>
2020-12-12 17:34:33 +0900

    failed => need to greedy 80 20 => 100

--- programmers/구명보트3.py
+++ programmers/구명보트3.py
@@ -2,7 +2,12 @@ import copy
 
 
 def solution(peoples, limit):
-    peoples.sort()
+    peoples.sort(reverse=True)
+    print(peoples)
+    boat = []
+    count = 0
+    for i in range(len(peoples)):
+        pass
 
 
 print(solution([70, 50, 80, 50], 100))
