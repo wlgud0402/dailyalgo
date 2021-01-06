607cfa04433635202adbad49c076b68acb869c89
wlgud0402 <wlgudrlgus@naver.com>
2021-01-07 00:25:03 +0900

    bfs dfs

---
+++ 백준/그래프탐색/dfs_bfs.py
@@ -0,0 +1,44 @@
+import sys
+from collections import deque
+
+sys.stdin = open('test.txt')
+N, M, start = map(int, input().split(" "))
+graph = {}
+for i in range(1, N+1):
+    graph[i] = []
+for _ in range(M):
+    s, e = map(int, input().split(" "))
+    graph[s].append(e)
+
+visited = set()
+stack = []
+stack.append(start)
+visited.add(start)
+
+dfs = []
+
+while len(stack) > 0:
+    curr = stack.pop()
+    dfs.append(str(curr))  # print(curr)
+    for adj in graph[curr]:
+        if adj not in visited:
+            stack.append(adj)
+            visited.add(adj)
+
+visited = set()
+queue = deque()
+queue.append(start)
+visited.add(start)
+
+bfs = []
+
+while len(queue) > 0:
+    curr = queue.popleft()
+    bfs.append(str(curr))
+    for adj in graph[curr]:
+        if adj not in visited:
+            queue.append(adj)
+            visited.add(adj)
+
+print(" ".join(dfs))
+print(" ".join(bfs))
