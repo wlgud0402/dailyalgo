50b20e13bcc159ec0696078d44f31a50ecb95c3d
wlgud0402 <wlgudrlgus@naver.com>
2021-01-11 15:29:13 +0900

    그래프에 방향이 있는지 확인하기

--- 백준/그래프탐색/바이러스.py
+++ 백준/그래프탐색/바이러스.py
@@ -1,6 +1,6 @@
 import sys
 
-# sys.stdin = open('바이러스.txt')
+sys.stdin = open('바이러스.txt')
 node_count = int(input())
 line_count = int(input())
 
@@ -11,7 +11,8 @@ for i in range(1, node_count+1):
 for _ in range(line_count):
     start, end = map(int, input().split(" "))
     graph[start].append(end)
-    graph[start].sort(reverse=True)
+    graph[end].append(start)
+    # graph[start].sort(reverse=True)
 
 visited = set()
 stack = []
@@ -23,7 +24,7 @@ dfs = []
 while len(stack) > 0:
     curr = stack.pop()
     dfs.append(str(curr))
-    print(dfs)
+    # print(dfs)
     for adj in graph[curr]:
         if adj not in visited:
             stack.append(adj)
