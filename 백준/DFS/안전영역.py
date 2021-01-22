9f7acecc3ba20dff380a70f5e028eb5d79a5a39a
wlgud0402 <wlgudrlgus@naver.com>
2021-01-22 18:45:44 +0900

    solved

---
+++ 백준/DFS/안전영역.py
@@ -0,0 +1,78 @@
+import sys
+import copy
+
+sys.stdin = open('안전영역.txt')
+
+N = int(sys.stdin.readline())
+
+matrix = []
+for _ in range(N):
+    row = list(map(int, sys.stdin.readline().split(" ")))
+    matrix.append(row)
+
+safe_area_counts = []
+waterHeight = 0
+
+while True:
+    _matrix = copy.deepcopy(matrix)
+    area_count = 0
+    stack = []
+    visited = set()
+
+    for i in range(N):
+        for j in range(N):
+            if _matrix[i][j] > waterHeight:
+                _matrix[i][j] = 0
+            else:
+                _matrix[i][j] = -1
+
+    print("초기 복사된 매트릭스", "물높이:", waterHeight)
+    # print(_matrix[0])
+    # print(_matrix[1])
+    # print(_matrix[2])
+    # print(_matrix[3])
+    # print(_matrix[4])
+
+    for i in range(N):
+        for j in range(N):
+            if _matrix[i][j] == 0:
+                stack.append((i, j))
+                visited.add((i, j))
+                # print("스택에 추가됨", stack)
+                while len(stack) > 0:
+                    r, c = stack.pop()
+                    # print(r, c)
+                    area_count += 1
+                    # print(area_count)
+                    if _matrix[r][c] == 0:
+                        _matrix[r][c] = -1
+                        print("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
+                        print(_matrix[0])
+                        print(_matrix[1])
+                        print(_matrix[2])
+                        print(_matrix[3])
+                        print(_matrix[4])
+
+                        for dr, dc in ((-1, 0), (0, 1), (1, 0), (0, -1)):
+                            next_r = r + dr
+                            next_c = c + dc
+
+                            if(
+                                0 <= next_r < N
+                                and 0 <= next_c < N
+                                and _matrix[next_r][next_c] == 0
+                                and (next_r, next_c) not in visited
+                            ):
+                                stack.append((next_r, next_c))
+                                visited.add((next_r, next_c))
+                                # print("stack added", stack)
+                print(area_count)
+            else:
+                continue
+    waterHeight += 1
+    safe_area_counts.append(area_count)
+    if area_count == 0:
+        flag = False
+        break
+
+print(safe_area_counts)
