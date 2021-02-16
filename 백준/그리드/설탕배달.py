0c40d29280e3779a506b432eef8de4ac9d0f0c4c
wlgud0402 <wlgudrlgus@naver.com>
2021-02-16 21:21:07 +0900

    faied

---
+++ 백준/그리드/설탕배달.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('설탕배달.txt')
+weight = int(sys.stdin.readline())
+
+bag_count = 0
+while weight != 0:
+    pass
+
+
+# 나머지를 이용해 계산
+# if 3, 5 모두로 나눠지지 않는다면 -1 => 결국 딱 맞게 설탕을 담을수 없다
+
+# if 3으로 나눈 나머지가 5로 나눌수있다면 나누기 가능
