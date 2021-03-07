8083a49b08665dc5e244badccf60d83887103949
wlgud0402 <wlgudrlgus@naver.com>
2021-03-07 23:37:04 +0900

    failed

---
+++ programmers/다트게임2.py
@@ -0,0 +1,21 @@
+import math
+# math.pow(input_number, 2))
+
+
+def solution(dartResult):
+    scores = []
+    points_and_cal = []
+
+    for i in range(len(dartResult)):
+        pass
+    answer = 0
+    # cal = {
+    #     "S": 1,
+    #     "D": 2,
+    #     "T": 3,
+    #     "*": 2,
+    #     "#": -1,
+    # }
+
+
+print(solution("1D2S#10S")  # 9
