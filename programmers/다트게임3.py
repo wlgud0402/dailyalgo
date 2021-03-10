795a267f57cb968b12ee1209ae2dbabedd371b8e
wlgud0402 <wlgudrlgus@naver.com>
2021-03-10 21:59:31 +0900

    dart game

---
+++ programmers/다트게임3.py
@@ -0,0 +1,14 @@
+import math
+# math.pow(input_number, 2))
+
+
+def solution(dartResult):
+    scores = []
+    points_and_cal = []
+
+    for i in range(len(dartResult)):
+        pass
+    answer = 0
+
+
+print(solution("1D2S#10S")  # 9
