76aac38c2a7c68605a4717e5bd84b6158500d390
wlgud0402 <wlgudrlgus@naver.com>
2021-04-15 00:06:04 +0900

    second success

---
+++ hackerRank/problem_solving/is_leap.py
@@ -0,0 +1,26 @@
+# def is_leap(year):
+#     if year % 4 == 0:
+#         if year % 100 == 0:
+#             if year % 400 == 0:
+#                 return True
+#             else:
+#                 return False
+#         else:
+#             return False
+#     else:
+#         return False
+
+def is_leap2(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+print(is_leap2(1992))  # True
