24272019dbffc36ab13b82fd00fedc8e7b6d2a9f
wlgud0402 <wlgudrlgus@naver.com>
2021-04-14 22:47:24 +0900

    wrong

---
+++ hackerRank/problem_solving/company_logo.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+
+def solution(s):
+    count = Counter(s)
+    count = count.most_common(3)
+    sorted_count = sorted(count, key=lambda x: (-x[1], x[0]))
+    # print(count)
+
+    for key, value in sorted_count:
+        print(key, value)
+
+
+print(solution("qwertyuiopasdfghjklzxcvbnm"))
+
+# a 1
+# b 1
+# c 1
