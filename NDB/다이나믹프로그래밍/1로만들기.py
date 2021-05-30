7285bed48972b37f435f90a846a07beb7917247e
wlgud0402 <wlgudrlgus@naver.com>
2021-05-30 17:53:40 +0900

    ndb prepare test

---
+++ NDB/다이나믹프로그래밍/1로만들기.py
@@ -0,0 +1,12 @@
+# 1로 만들기 최소 연산횟수로
+# 가능한 연산
+# 1. if n % 5 == 0: n == n//5
+# 2. if n % 3 == 0: n == n//3
+# 3. if n % 2 == 0: n == n//2
+# 4. else: n-=1
+def solution(n):
+    dp = [0] * 30001
+    pass
+
+
+print(solution(26))  # 3
